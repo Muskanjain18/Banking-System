4cea5fa5ae8a9a0bb271402323213f1372e3f96f
Muskanjain18 <62690472+Muskanjain18@users.noreply.github.com>
2021-07-11 23:03:57 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,49 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" type="text/css" href="navbar.css">
+
+    <title>SERVICE BANK</title>
+  </head>
+
+  <body>
+  <?php
+  include 'navbar.php';
+  ?>
+
+<br><br><br>
+    
+      <!-- Activity section -->
+            <div class="row activity text-center" style="background-image: url('https://i.pinimg.com/originals/74/43/a3/7443a31b6a19e5630b94a6538ff9bdd1.gif');">
+                  <!--<div class="col-md act">
+                    <img src="users.png" class="img-fluid">
+                    <br>
+                    <a href="createuser.php"><button>Create a User</button></a>
+                  </div> -->
+                  <div class="col-md act">
+                    <img src="moneytransfer.png" class="img-fluid">
+                    <br>
+                    <a href="transfermoney.php"><button>Make a Transaction</button></a>
+                  </div>
+                  <div class="col-md act">
+                    <img src="history2.jpg" class="img-fluid">
+                    <br>
+                    <a href="transactionhistory.php"><button>Transaction History</button></a>
+                  </div>
+            </div>
+      </div>
+      <br><br><br>
+      <footer class="text-center mt-5 py-2">
+        <p>&copy JULY 2021. Made by <b>MUSKAN JAIN</b> <br> The Sparks Foundation</p>
+      </footer>
+      <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+  </body>
+</html>
